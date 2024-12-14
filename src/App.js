763922e2960761ac406a30c4e8ff37d025953e63
Erlow <erlowgaming@gmail.com>
2024-12-14 14:33:18 +0100

    Fix new design

--- src/App.js
+++ src/App.js
@@ -6,28 +6,28 @@ import Projects from "./routes/Projects";
 import Experiences from "./routes/Experiences";
 import TroisDimension from './routes/TroisDimension';
 import { Routes, Route } from 'react-router-dom';
-import TOPOLOGY from 'vanta/src/vanta.topology';
 import { useEffect } from 'react';
 
 function App() {
-
   useEffect(() => {
     const colors = [0x4d4d4d, 0x336699, 0x993333, 0x339966, 0x663399, 0x996633];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
 
-    const vantaEffect = TOPOLOGY({
-      el: '#vanta',
-      mouseControls: true,
-      touchControls: true,
-      gyroControls: false,
-      minHeight: 200.0,
-      minWidth: 200.0,
-      scale: 1.0,
-      scaleMobile: 1.0,
-      color: randomColor,
-      backgroundColor: 0x0,
-    })
-  })
+    import('vanta/src/vanta.topology').then((VANTA) => {
+      VANTA.default({
+        el: '#vanta',
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.0,
+        minWidth: 200.0,
+        scale: 1.0,
+        scaleMobile: 1.0,
+        color: randomColor,
+        backgroundColor: 0x0,
+      });
+    });
+  }, []);
 
   return (
       <div className="App">
